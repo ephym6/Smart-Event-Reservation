0296d05598b88e94ba233ab2658000d6ecf3eee7
Ian Kabachia <zax19567@gmail.com>
2025-11-12 02:10:18 +0300

    model seeders for test data home page and dashboard added

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/dashboard', 'dashboard')->name('dashboard');
 
 Route::get('/test', function () {
     return 'Test works!';
