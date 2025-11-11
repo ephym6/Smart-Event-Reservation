570d5ef23aebc69ad60b16ac20134857a007667d
Ephy Macharia <ephy.macharia.mbuu@gmail.com>
2025-11-11 21:39:25 +0300

    model seeders for test data

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Venue;
+use App\Models\Event;
+use App\Models\InventoryItem;
+use App\Models\Reservation;
+use App\Models\ReservationItem;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)->create();
+        Venue::factory(5)->create();
+        Event::factory(20)->create();
+        InventoryItem::factory(30)->create();
+        Reservation::factory(15)->create();
+        ReservationItem::factory(40)->create();
     }
 }
